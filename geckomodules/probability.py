5c54059ba93d48bf6d2359937ed57eff139978a3
John <john.e.dougherty.ii@gmail.com>
2012-07-02 19:07:56 -0700

    many new probability problems

--- geckomodules/probability.py
+++ geckomodules/probability.py
@@ -2,6 +2,7 @@ from __future__ import division
 
 import collections
 import fractions
+import logging
 import random
 
 
@@ -26,6 +27,16 @@ SCENARIOS = [
     }
 ]
 
+
+def latex(fraction):
+    if fraction.denominator == 1:
+        texed = str(fraction)
+    else:
+        return r'\ensuremath{{\frac{{{N}}}{{{D}}}}}'.format(
+            N=fraction.numerator, D=fraction.denominator
+        )
+    
+    return texed
 
 class TwoVarChanceProb(Problem):
     '''A probability problem of two variables.
@@ -272,7 +283,407 @@ class TwoUrnProb(Problem):
         ) 
         return text
 
+class NofMProb(Problem):
+    '''A probability problem involving choices.
 
+    There are M options, everyone must choose N of them or none at all.
+    '''
+    printable = True
+    secname = 'P5-type'
+    flavors = (
+        {'statement':
+         r'''An insurer offers a health plan to the employees of a large
+         company.  As part of this plan, the individual employees may choose
+         exactly two of the supplementary coverages A, B, and C, or they may
+         choose no supplementary coverage.  The proportions of the company's
+         employees that choose coverages A, B, and C are {PROPA}, {PROPB}, and
+         {PROPC}, respectively.  Determine the probability that a randomly
+         chosen employee will choose no supplementary coverage.
+         ''',
+         'solution': 
+         r'''For simplicity, let's work with numbers of people, instead of
+         proportions.  The g.c.d.~of the three proportions is {GCD}, so suppose
+         there are {GCD} people.  Then {CHOOSEA} chose A, {CHOOSEB} chose B,
+         and {CHOOSEC} chose C, and {CHOICES} choices were made in total.  But
+         each person made 2 choices, so {PCHOSE} chose to take supplementary
+         coverage.  This leaves {CHOOSEN} of {GCD} people who made no choice,
+         or a proportion of {ANSWER}.  
+         '''
+        },
+        {'statement':
+         r'''An ice cream store sells single- and double-scoop ice cream cones
+         in vanilla, chocolate, and strawberry flavors.  Customers who order a
+         double-scoop must choose exactly two of these flavors.  {PROPA} of the
+         customers choose a double-scoop with vanilla, {PROPB} choose a
+         double-scoop with chocolate, and {PROPC} choose a double-scoop with
+         strawberry.  Determine the probability that a randomly
+         chosen customer will choose a single-scoop ice cream cone.
+         ''',
+         'solution': 
+         r'''For simplicity, let's work with numbers of people, instead of
+         proportions.  The g.c.d.~of the three proportions is {GCD}, so suppose
+         there are {GCD} people.  Then {CHOOSEA} chose vanilla, {CHOOSEB} chose
+         chocolate, and {CHOOSEC} chose strawberry, and {CHOICES} choices were
+         made in total.  But each person made 2 choices, so {PCHOSE} chose to
+         take a double-scoop.  This leaves {CHOOSEN} of {GCD} people
+         who took a single-scoop, or a proportion of {ANSWER}.  
+         '''
+        }
+    ,)
+
+    def __init__(self, n=2, m=3, max_denominator=20):
+        super(NofMProb, self).__init__()
+        
+        self.flavor = random.choice(self.flavors)
+        choices = collections.defaultdict(int)
+        choices['None'] = random.randrange(1,max_denominator)
+        for person in xrange(max_denominator - choices['None']):
+            for choice in random.sample(('A', 'B', 'C'), 2):
+                choices[choice] += 1
+
+        self.propA = fractions.Fraction(choices['A'],max_denominator)
+        self.propB = fractions.Fraction(choices['B'],max_denominator)
+        self.propC = fractions.Fraction(choices['C'],max_denominator)
+        self.propNone = fractions.Fraction(choices['None'],max_denominator)
+
+        self.statement = self.flavor['statement'].format(
+            PROPA=latex(self.propA), PROPB=latex(self.propB),
+            PROPC=latex(self.propC)
+        )
+
+        self.answer = latex(self.propNone)
+
+    @property
+    def solution(self):
+
+        gcd = reduce(fractions.gcd, 
+                     (self.propA, self.propB, self.propC)).denominator 
+        choices = {}
+        choices['A'] = self.propA * gcd
+        choices['B'] = self.propB * gcd
+        choices['C'] = self.propC * gcd
+        total_choices = sum(choices.values())
+        choices['None'] = self.propNone * gcd
+
+        
+
+        text = self.flavor['solution'].format(
+            GCD=gcd, CHOOSEA=choices['A'], CHOOSEB=choices['B'],
+            CHOOSEC=choices['C'], CHOICES=total_choices,
+            PCHOSE=(total_choices/2), CHOOSEN=choices['None'],
+            ANSWER=self.answer
+        )
+
+        return text
+
+class ConditionalProb(Problem):
+    '''A probability problem for DeMorgan's laws.'''
+    printable = True
+    secname = 'P7-type'
+    def __init__(self):
+        super(ConditionalProb, self).__init__()
+
+        self.probs = {}
+        self.probs['D'] = random.randrange(1, 30) / 100
+        self.probs['S'] = random.randrange(1, self.probs['D']*100) / 100
+        self.probs['T'] = random.randrange(1, 10) / 100
+
+        self.statement = r'''An actuary is studying the prevalence of three
+        health risk factors, denoted by A, B, and C, within a population of
+        women.  For each of the three factors, the probability is {SPROB} that
+        a woman in the population has only this risk factor (and no others).
+        For any two of the three facts, the probability is {DPROB} that she has
+        exactly these two risk factors (but not the other).  The probability
+        that a woman has all three risk factors, given that she has A and B,
+        is {PPROB:.2f}.  What is the probability that a woman has none of
+        the three risk factors, given that she does not have risk factor A?
+        '''.format(
+            SPROB=self.probs['S'], DPROB=self.probs['D'],
+            PPROB=(self.probs['T']/(self.probs['T'] + self.probs['D']))
+        )
+        self.answer = (
+            (1-(3*self.probs['S'] + 3*self.probs['D'] + self.probs['T']))/
+            (1 - (self.probs['S'] + 2*self.probs['D'] + self.probs['T']))
+        )
+
+    @property
+    def solution(self):
+        text = r'''This Venn Diagram describes the problem:
+        \[
+        \text{{not yet implemented}}
+        \]
+        We are given
+        \[
+        P(A \cap B' \cap C') = P(A' \cap B \cap C') = P(A' \cap B' \cap C) =
+        {SPROB:.2f}
+        \]
+        (having exactly one risk factor means not having either of the other
+        two).  We are also given
+        \[
+        P(A \cap B \cap C') = P(A \cap B' \cap C) = P(A' \cap B \cap C) =
+        {DPROB:.2f}.
+        \]
+        Finally, we are given
+        \[
+        P(A \cap B \cap C \mid A \cap B) = {PPROB:.2f}.
+        \]
+        Then
+        \[
+        P(A \cap B \cap C \mid A \cap B) = \frac{{P(A \cap B \cap C)}}{{P(A \cap
+        B)}} = {PPROB:.2f}
+        \]
+        so
+        \[
+        P(A \cap B \cap C) = {PPROB:.2f} \cdot P(A \cap B).
+        \]
+        Referring to the diagram, let $P(A \cap B \cap C) = x$, so that $P(A
+        \cap B) = x + {DPROB:.2f}$.  Then
+        \[
+        x = P(A \cap B \cap C) = {PPROB:.2f} \cdot P(A \cap B) =
+        {PPROB:.2f}(x+{DPROB:.2f})
+        \]
+        So $x = {TPROB:.2f}$.  The four regions of A then sum to $P(A) =
+        {SPROB:.2f} + {DPROB:.2f}+ {DPROB:.2f} + {TPROB:.2f} = {APROB:.2f}$, so
+        $P(A') = 1 - P(A) = {APPROB:.2f}$.  The problem asks for
+        \[
+        P(A' \cap B' \cap C' \mid A') = \frac{{P(A' \cap B' \cap C')}}{{P(A')}}
+        = \frac{{P(A' \cap B' \cap C')}}{{{APPROB:.2f}}}
+        \]
+        And the numerator is just the complement of all the numbered regions:
+        \[
+        P(A' \cap B' \cap C' \mid A') = \frac{{1-(3 \times {SPROB:.2f} + 3
+        \times {DPROB:.2f} + {TPROB:.2f})}}{{{APPROB:.2f}}} =
+        \frac{{{NUM}}}{{{APPROB:.2f}}} = {ANS:.3f}
+        \]
+        '''.format(
+            SPROB=self.probs['S'], DPROB=self.probs['D'],
+            TPROB=self.probs['T'],
+            PPROB=(self.probs['T']/(self.probs['T'] + self.probs['D'])),
+            APROB=(self.probs['S'] + 2*self.probs['D'] + self.probs['T']),
+            APPROB=(1 - 
+                    (self.probs['S'] + 2*self.probs['D'] + self.probs['T'])),
+            NUM=(1-(3*self.probs['S'] + 3*self.probs['D'] + self.probs['T'])),
+            ANS=self.answer
+        )
+        return text
+
+def percent(num):
+    'A LaTeX-safe way of outputting a percentage'
+    return ''.join([str(int(num * 100)), r'\%'])
+
+class TwoVarProb(Problem):
+    '''A two-variable probability problem.
+
+    I am having the darndest time doing this synthetically, so I'm just going
+    to have to do it analytically.  Hopefully this class will one day subsume
+    TwoUrnProb.  Naming conventions: [ (L (M) R) ]
+    '''
+    printable = True
+    secname = 'Two Variable Probability'
+    ptypes = (
+        {'statement': r'''The probability that a visit to a primary care
+         physician's (PCP) office results in neither lab work nor referral to a
+         specialist is {PPnLAnR}.  Of those coming to a PCP's office, {PPR} are
+         referred to specialists and {PPL} require lab work.  Determine the
+         probability that a visit to a PCP's office results in both lab work
+         and referral to a specialist.
+         ''',
+         'answer': '{PLAR}',
+         'solution': r'''Label the classes like so:
+         \begin{{itemize}}
+         \item[$L$:] lab work needed
+         \item[$R$:] referral to a specialist given
+         \end{{itemize}}
+         We are given $P(L' \cap R') = {PnLAnR}$, $P(R) ={PR}$, and $P(L)={PL}$.
+         It follws that $P(L \cup R) = 1 - P(L' \cap R') = {PLOR}$, and then
+         \[
+         P(L \cap R) = P(L) + P(R) - P(L \cup R) = {PL} + {PR} - {PLOR} =
+         {PLAR}.
+         \]
+         So the probability of both is ${PLAR}$.
+         ''',
+         'ltest':  lambda: True if random.gauss(.3, .3) > .5 else False,
+         'rtest':  lambda: True if random.gauss(.3, .3) > .5 else False,
+        },
+        {'statement': r'''You are given $P(A \cup B) = {PLOR}$ and $P(A \cup
+         B') = {PLOnR}$.  Determine $P(A)$.
+         ''',
+         'answer': '{PL}',
+         'solution': r'''$P(A \cup B) = P(A) + P(B) - P(A \cap B)$ and $P(A \cup
+         B') = P(A) + P(B') - P(A \cap B')$.  We use the relationship $P(A) =
+         P(A \cap B) + P(A \cap B')$.  Then
+         \begin{{align*}}
+         P(A \cup B) + P(A \cup B') 
+         &= P(A) + P(B) - P(A \cap B) \\&\phantom{{=}}+ P(A) + P(B') - P(A \cap B')\\
+         &= 2P(A) + 1 - P(A) = P(A) + 1
+         \end{{align*}}
+         Since $P(B) + P(B') = 1$.  Therefore, ${PLOR} + {PLOnR} = P(A) + 1$ so
+         that $P(A) = {PL}$.
+         ''',
+         'ltest': lambda: True if random.gauss(.3, .3) > .5 else False, 
+         'rtest': lambda: True if random.gauss(.3, .3) > .5 else False, 
+        },
+    )
+
+    def __init__(self, U=100):
+
+        self.ptype = random.choice(self.ptypes)
+
+        self.L = set()
+        self.R = set()
+        self.U = set(xrange(U))
+        for entry in self.U:
+            ladd = self.ptype['ltest']()
+            if ladd:
+                self.L.add(entry)
+            radd = self.ptype['rtest']()
+            if radd:
+                self.R.add(entry)
+
+        values = {
+            'PL': len(self.L)/len(self.U), 
+            'PR': len(self.R)/len(self.U),
+            'PnL': len(self.U - self.L)/len(self.U),
+            'PnR': len(self.U - self.R)/len(self.U),
+            'PLAR': len(self.L & self.R)/len(self.U),
+            'PnLAR': len((self.U - self.L) & self.R)/len(self.U),
+            'PLAnR': len(self.L & (self.U - self.R))/len(self.U),
+            'PnLAnR': len((self.U - self.L) & (self.U - self.R))/len(self.U),
+            'PLOR': len(self.L | self.R)/len(self.U),
+            'PnLOR': len((self.U - self.L) | self.R)/len(self.U),
+            'PLOnR': len(self.L | (self.U - self.R))/len(self.U),
+            'PnLOnR': len((self.U - self.L) | (self.U - self.R))/len(self.U),
+        }
+
+        for key in values.keys():
+            values['P'+key] = percent(values[key])
+
+        self.statement = self.ptype['statement'].format(**values)
+
+        self.answer = self.ptype['answer'].format(**values)
+
+        self.solution = self.ptype['solution'].format(**values)
+
+class ThreeVarProb(Problem):
+    '''A three-variable probability problem.
+
+    This ought to be combined with TwoVarProb into an Nvar prob.
+    '''
+    printable = True
+    secname = 'Three Variable Probability'
+    ptypes = (
+        {'statement': r'''A survey of a group's viewing habits over the last
+         year revealed the following information:
+         \begin{{itemize}}
+         \item {PPA} watched gymnastics
+         \item {PPB} watched baseball
+         \item {PPC} watched soccer
+         \item {PPAAB} watched gymnastics and baseball
+         \item {PPBAC} watched baseball and soccer
+         \item {PPAAC} watched gymnastics and soccer
+         \item {PPAABAC} watched all three sports
+         \end{{itemize}}
+         Calculate the percentage of the group that watched none of the three
+         sports during the last year.
+         ''',
+         'answer': '{PPnAAnBAnC}',
+         'solution': r'''Label the classes as follows:
+         \begin{{itemize}}
+         \item[$G$:] watched gymnastics
+         \item[$B$:] watched baseball
+         \item[$S$:] watched soccer
+         \end{{itemize}}
+         We need to find $P(G' \cap B' \cap S')$.  By DeMorgan's law we have
+         \[
+         P(G' \cap B' \cap S') = 1 - P(G \cup B \cup S).
+         \]
+         We use the relationship
+         \begin{{align*}}
+         P(G \cup B \cup S) &= P(G) + P(B) + P(S) \\&\phantom{{=}}- (P(G \cap B) + P(G \cap S) +
+         P(B \cap S)) \\&\phantom{{=}}+ P(G \cap B \cap S)
+         \end{{align*}}
+         and the values from the statement:
+         \begin{{itemize}}
+         \item $P(G) = {PPA}$
+         \item $P(B) = {PPB}$
+         \item $P(S) = {PPC}$
+         \item $P(G\cap B) = {PPAAB}$
+         \item $P(B\cap S) = {PPBAC}$
+         \item $P(G\cap S) = {PPAAC}$
+         \item $P(G\cap B\cap S) = {PPAABAC}$
+         \end{{itemize}}
+         Then $P(G \cup B \cup S) = {PAOBOC}$ and $P(G' \cap B' \cap S') = 1 -
+         {PAOBOC} = {PnAAnBAnC}$.
+         ''',
+         'ltest':  lambda: True if random.gauss(.3, .3) > .5 else False,
+         'rtest':  lambda: True if random.gauss(.3, .3) > .5 else False,
+        },
+    )
+
+    def __init__(self, U=100):
+
+        self.ptype = random.choice(self.ptypes)
+
+        self.A = set()
+        self.B = set()
+        self.C = set()
+        self.U = set(xrange(U))
+        for entry in self.U:
+            for group in (self.A, self.B, self.C):
+                if self.ptype['rtest']():
+                    group.add(entry)
+
+        values = {
+            'PA': len(self.A)/len(self.U),
+            'PB': len(self.B)/len(self.U),
+            'PC': len(self.C)/len(self.U),
+            'PnA': len((self.U - self.A))/len(self.U),
+            'PnB': len((self.U - self.B))/len(self.U),
+            'PnC': len((self.U - self.C))/len(self.U),
+            'PAAB': len(self.A & self.B)/len(self.U),
+            'PAAC': len(self.A & self.C)/len(self.U),
+            'PAAnB': len(self.A & (self.U - self.B))/len(self.U),
+            'PAAnC': len(self.A & (self.U - self.C))/len(self.U),
+            'PBAC': len(self.B & self.C)/len(self.U),
+            'PBAnA': len(self.B & (self.U - self.A))/len(self.U),
+            'PBAnC': len(self.B & (self.U - self.C))/len(self.U),
+            'PCAnA': len(self.C & (self.U - self.A))/len(self.U),
+            'PCBnB': len(self.C & (self.U - self.B))/len(self.U),
+            'PAABAC': len(self.A & self.B & self.C)/len(self.U),
+            'PAABAnC': len(self.A & self.B & (self.U - self.C))/len(self.U),
+            'PAAnBAC': len(self.A & (self.U - self.B) & self.C)/len(self.U),
+            'PAAnBAnC': len(self.A & (self.U - self.B) & (self.U - self.C))/len(self.U),
+            'PnAABAC': len((self.U - self.A) & self.B & self.C)/len(self.U),
+            'PnAABAnC': len((self.U - self.A) & self.B & (self.U - self.C))/len(self.U),
+            'PnAAnBAC': len((self.U - self.A) & (self.U - self.B) & self.C)/len(self.U),
+            'PnAAnBAnC': len((self.U - self.A) & (self.U - self.B) & (self.U - self.C))/len(self.U),
+            'PAOB': len(self.A | self.B)/len(self.U),
+            'PAOC': len(self.A | self.C)/len(self.U),
+            'PAOnB': len(self.A | (self.U - self.B))/len(self.U),
+            'PAOnC': len(self.A | (self.U - self.C))/len(self.U),
+            'PBOC': len(self.B | self.C)/len(self.U),
+            'PBOnA': len(self.B | (self.U - self.A))/len(self.U),
+            'PBOnC': len(self.B | (self.U - self.C))/len(self.U),
+            'PCOnA': len(self.C | (self.U - self.A))/len(self.U),
+            'PCOnB': len(self.C | (self.U - self.B))/len(self.U),
+            'PAOBOC': len(self.A | self.B | self.C)/len(self.U),
+            'PAOBOnC': len(self.A | self.B | (self.U - self.C))/len(self.U),
+            'PAOnBOC': len(self.A | (self.U - self.B) | self.C)/len(self.U),
+            'PAOnBOnC': len(self.A | (self.U - self.B) | (self.U - self.C))/len(self.U),
+            'PnAABAC': len((self.U - self.A) & self.B & self.C)/len(self.U),
+            'PnAABAnC': len((self.U - self.A) & self.B & (self.U - self.C))/len(self.U),
+            'PnAAnBAC': len((self.U - self.A) & (self.U - self.B) & self.C)/len(self.U),
+            'PnAAnBAnC': len((self.U - self.A) & (self.U - self.B) & (self.U - self.C))/len(self.U),
+        }
+
+        for key in values.keys():
+            values['P'+key] = percent(values[key])
+
+        self.statement = self.ptype['statement'].format(**values)
+
+        self.answer = self.ptype['answer'].format(**values)
+
+        self.solution = self.ptype['solution'].format(**values)
 
 if __name__ == '__main__':
     # add something here!
